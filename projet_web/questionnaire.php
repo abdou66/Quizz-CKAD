4e25fea3bbc94d75bca35345f99a4751d397f54a
Jeylani66 <34726536+abdou66@users.noreply.github.com>
2019-02-17 18:46:52 +0100

    Add files via upload

--- projet_web/questionnaire.php
+++ projet_web/questionnaire.php
@@ -1,12 +1,16 @@
-<?php   
+<?php
 include 'connexion_base.php';
 session_start();
+$userChoice = $_POST["reponse"];   //on recupere ce que le serveur a envoyé (i.e le choix de l'utilisateur)
 // $id=1;
- // $sql = "SELECT * FROM questions WHERE refQuestionnaire=:ref";
-		$sql=("SELECT * FROM questions");
-		$data = array();
-		//while($row=$sql->fetch()){
-			foreach($bdd->query($sql, PDO::FETCH_ASSOC) as $row)
-			$data[]=$row; 
-			echo json_encode($data);
+if(isset($userChoice)){
+    $prepInsert = $bdd->prepare("INSERT INTO reponses (choix) VALUES (:choix)");
+    $result = $prepInsert->execute(array("choix"=> $userChoice));  
+   ///////------------------------------------ // $sql = "SELECT * FROM questions WHERE refQuestionnaire=:ref";
+    $sql=("SELECT * FROM questions");
+    $data = array();
+        foreach($bdd->query($sql, PDO::FETCH_ASSOC) as $row)
+        $data[]=$row; 
+        echo json_encode($data);
+} 
 ?>
